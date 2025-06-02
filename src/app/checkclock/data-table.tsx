49fba349f95f0203aaa35cf33bc11841dea78ca3
lucky kurniawan <luckykrnwn17@gmail.com>
2025-06-02 15:02:40 +0700

    feat: refactor checkclock components by removing unused imports and adding alert dialog

--- src/app/checkclock/data-table.tsx
+++ src/app/checkclock/data-table.tsx
@@ -43,7 +43,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+
 
 import { Select } from "@radix-ui/react-select";
 import {
