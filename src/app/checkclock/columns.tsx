471bf6af29b53cfd8d5e487f9f9c74f2a97e2953
lucky kurniawan <luckykrnwn17@gmail.com>
2025-06-02 14:57:56 +0700

    feat: add alert dialog for HR rejection reason in checkclock component

--- src/app/checkclock/columns.tsx
+++ src/app/checkclock/columns.tsx
@@ -234,9 +234,34 @@ export const columns: ColumnDef<CheckclockOverview>[] = [
       if (!showButton) return null;
       return (
         <div className="w-full justify-center flex">
-          <Button variant={"outline"} size={"sm"}>
-            {reason ? reason : "View"}
-          </Button>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant={"outline"} size={"sm"}>
+                {reason ? reason : "View"}
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle className="flex gap-3">
+                  <div className="h-full justify-center flex items-center">
+
+                  <svg width="20" height="20" viewBox="0 0 101 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" width="100" height="100" rx="50" fill="currentColor"/>
+<path d="M50.5135 72.012C48.9349 72.012 47.7189 71.564 46.8655 70.668C46.0549 69.7293 45.6495 68.428 45.6495 66.764V44.876C45.6495 43.1693 46.0549 41.868 46.8655 40.972C47.7189 40.0333 48.9349 39.564 50.5135 39.564C52.0495 39.564 53.2229 40.0333 54.0335 40.972C54.8869 41.868 55.3135 43.1693 55.3135 44.876V66.764C55.3135 68.428 54.9082 69.7293 54.0975 70.668C53.2869 71.564 52.0922 72.012 50.5135 72.012ZM50.5135 34.316C48.7215 34.316 47.3349 33.9107 46.3535 33.1C45.4149 32.2467 44.9455 31.052 44.9455 29.516C44.9455 27.9373 45.4149 26.7427 46.3535 25.932C47.3349 25.0787 48.7215 24.652 50.5135 24.652C52.3055 24.652 53.6709 25.0787 54.6095 25.932C55.5482 26.7427 56.0175 27.9373 56.0175 29.516C56.0175 31.052 55.5482 32.2467 54.6095 33.1C53.6709 33.9107 52.3055 34.316 50.5135 34.316Z" fill="white"/>
+</svg>
+                  </div>
+
+                  Reason for HR Rejection
+                  </AlertDialogTitle>
+                <AlertDialogDescription>
+                  During working hours you are not in the office
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel className="w-auto">Cancel</AlertDialogCancel>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </div>
       );
     },
